fc531cc9b3dfc88d311d6a2a31a326647d6fe3ab
Anthony saldana <anthonywebsol@gmail.com>
2025-01-01 23:06:24 -0800

    loosen category restrictions with worker

--- src/workers/inventoryWorker.ts
+++ src/workers/inventoryWorker.ts
@@ -1,6 +1,7 @@
 import { Job } from 'bull';
 import mealmeapi from '@api/mealmeapi';
 import { InventoryItem, InventoryItemType } from '../models/grocery';
+// import OpenAI from 'openai';
 
 export const processInventoryJob = async (job: Job) => {
   const { store_id, subcategory_id, latitude, longitude, user_street_num, user_street_name, user_city, user_state, user_zipcode, user_country } = job.data;
@@ -24,10 +25,47 @@ export const processInventoryJob = async (job: Job) => {
       user_country: user_country as string
     });
 
+    console.log(response.data, 'response.data');
+
     const inventoryItems: InventoryItemType[] = [];
+
+    // Use OpenAI to filter categories
+    // const openai = new OpenAI({
+    //   apiKey: process.env.OPENAI_API_KEY
+    // });
+    // const completion = await openai.chat.completions.create({
+    //   model: "gpt-4o-mini",
+    //   messages: [
+    //     {
+    //       role: "system",
+    //       content: `You are helping identify grocery categories. 
+    //       Return only an array of category IDs as json that you determine to be actual grocery categories,
+    //       excluding non-grocery items like prepared foods, alcohol, tobacco, etc.
+    //       Allow for trending and best seller categories. Return as a json array of category IDs.`
+    //     },
+    //     {
+    //       role: "user",
+    //       content: JSON.stringify(response.data.categories.map((cat: any) => ({
+    //         id: cat.subcategory_id,
+    //         name: cat.name
+    //       })))
+    //     }
+    //   ],
+    //   temperature: 0.3,
+    //   response_format: { type: "json_object" }
+    // });
+
+    // console.log(completion.choices[0].message.content, 'completion.choices[0].message.content');
+
+    // const groceryCategoryIds = JSON.parse(completion.choices[0].message.content!)?.category_ids || [];
 
     // Process items in current category
     await Promise.all(response.data.categories.flatMap(async (category: any) => {
+      // Only process grocery categories
+      // if (!groceryCategoryIds.includes(category.subcategory_id)) {
+      //   return;
+      // }
+
       // Add subcategories to queue if they exist
       if (category.menu_item_list.length === 0 && category.subcategory_id) {
         await job.queue.add({
@@ -49,10 +87,10 @@ export const processInventoryJob = async (job: Job) => {
 
       category.menu_item_list.map((item: any) => {
         inventoryItems.push({
-        store_id,
-        product_id: item.product_id,
-        name: item.name,
-        price: item.price / 100, // Convert cents to dollars
+          store_id,
+          product_id: item.product_id,
+          name: item.name,
+          price: item.price / 100, // Convert cents to dollars
           unit_size: item.unit_size,
           unit_of_measurement: item.unit_of_measurement,
           description: item.description,
@@ -61,9 +99,6 @@ export const processInventoryJob = async (job: Job) => {
         });
       });
     }));
-
-    // console.log("Inventory items: ");
-    // console.log(inventoryItems);
 
     // Store inventory items in the database
     // Upsert inventory items - update if exists, insert if new
